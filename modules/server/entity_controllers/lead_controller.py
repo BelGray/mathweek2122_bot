07960c08c73ebeb98e849dba0c7378108790a7a9
BelGray <goldendaniel186@gmail.com>
2024-02-21 18:01:10 +0300

    update entity controllers

---
+++ modules/server/entity_controllers/lead_controller.py
@@ -0,0 +1,41 @@
+import aiohttp
+
+from modules.server.data.enums import HTTPMethods
+from modules.server.entity_controllers.main_controller import ServerRequests
+from modules.server.data.enums import Subjects
+
+
+class LeadBoardController(ServerRequests):
+    @ServerRequests.request_log(HTTPMethods.GET)
+    async def get_leaderboard_by_subject_and_class_number(self, subject: Subjects,
+                                                          class_number: int) -> aiohttp.ClientResponse:
+        """Получить таблицу лидеров учеников по предмету и параллели классов"""
+        endpoint = f"leadBoard.getBySubjectAndClassNumber?classNumber={class_number}&subject={subject.value}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=super().url + endpoint) as response:
+                return response
+
+    @ServerRequests.request_log(HTTPMethods.GET)
+    async def get_leaderboard_by_subject_and_class_number_and_class_letter(self, subject: Subjects,
+                                                                           class_number: int, class_letter: str) -> aiohttp.ClientResponse:
+        """Получить таблицу лидеров учеников определенного класса по предмету"""
+        endpoint = f"leadBoard.getBySubjectAndClassNumberAndClassLetter?classNumber={class_number}&classLetter={class_letter}&subject={subject.value}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=super().url + endpoint) as response:
+                return response
+
+    @ServerRequests.request_log(HTTPMethods.GET)
+    async def get_leaderboard_by_class_number(self, class_number: int) -> aiohttp.ClientResponse:
+        """Получить таблицу лидеров по баллам параллели классов"""
+        endpoint = f"leadBoard.getByClassNumber?classNumber={class_number}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=super().url + endpoint) as response:
+                return response
+
+    @ServerRequests.request_log(HTTPMethods.GET)
+    async def get_leaderboard_by_class_number_and_class_letter(self, class_number: int, class_letter: str) -> aiohttp.ClientResponse:
+        """Получить таблицу лидеров по баллам определенного класса"""
+        endpoint = f"leadBoard.getByClassNumberAndClassLetter?classNumber={class_number}&classLetter={class_letter}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=super().url + endpoint) as response:
+                return response
