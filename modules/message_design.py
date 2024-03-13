198c06ec67accce70dcf22addce5173aa8a1fe67
BelGray <goldendaniel186@gmail.com>
2024-03-13 20:05:54 +0300

    change message_design.py

--- modules/message_design.py
+++ modules/message_design.py
@@ -91,7 +91,7 @@ class MessageDrawer:
         text = (
             f'📌 <b>Задание на тему "{topic}"</b>\n<i>{"Текстовая задача" if task_type == TaskTypes.WORD_TASK.value else "Выражение"}.'
             f' {tasks_levels[level]["label"]}</i>\n\n<blockquote>{text}</blockquote>\n\n🎯 <b>Вес задания: {tasks_levels[level]["points"]}</b>'
-            f'\n{status.value} <b>Статус задания: {status_str}</b>\n💬 <b>Ответ: {answer if answer is not None else " - "}</b>')
+            f'\n\n{status.value} <b>{status_str}</b>\n💬 <b>Ответ: {answer if answer is not None else " - "}</b>')
 
         return text
 
